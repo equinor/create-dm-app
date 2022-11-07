91ce0a2aab62e85ea476485fb4d5b61df2eb7d62
KristianKjerstad <kristian.kjerstad@bouvet.no>
2022-11-07 11:10:48 +0100

    feat: add new page "blueprint interaction" to query blueprints in demo app

--- src/plugins/demo-app/Routes.tsx
+++ src/plugins/demo-app/Routes.tsx
@@ -1,15 +1,20 @@
 import { TRoute } from '@development-framework/dm-core'
 import {Overview} from "./pages/Overview";
 import {Jobs} from "./pages/JobExample";
+import {BlueprintInteraction} from "./pages/BlueprintInteraction";
 
 const Routes: Array<TRoute> = [
-      {
+  {
     path: '/jobs',
     content: Jobs,
   },
   {
     path: '/',
     content: Overview,
+  },
+  {
+    path: '/blueprint-interaction',
+    content: BlueprintInteraction,
   },
 
 ]
---
+++ src/plugins/demo-app/pages/BlueprintInteraction.tsx
@@ -0,0 +1,37 @@
+import {
+  AuthContext,
+  DmssAPI,
+    JsonView
+} from '@development-framework/dm-core'
+import React, {useContext, useEffect, useState} from 'react'
+import {SingleSelect} from '@equinor/eds-core-react'
+import {AxiosResponse} from "axios";
+
+export const BlueprintInteraction = () => {
+  const packageName = "CarPackage"
+  const dataSource = 'DemoApplicationDataSource'
+  const { token } = useContext(AuthContext)
+  const dmssApi = new DmssAPI(token)
+  const [blueprints, setBlueprints] = useState<any[]>([])
+  const [selectedBlueprint, setSelectedBlueprint] = useState<object>({})
+
+  useEffect(() => {
+      fetchBlueprintsFromPackage(packageName)
+    }, [])
+
+  const fetchBlueprintsFromPackage = (pacakgeName: string) => {
+      dmssApi.documentGetByPath({dataSourceId: dataSource, path: `models/${pacakgeName}`})
+          .then((response: AxiosResponse<any> ) => {
+              setBlueprints(response.data.content)
+          })
+  }
+
+ return <div>
+        <p>
+            Select a blueprint inside the package: {packageName}
+        </p>
+        <SingleSelect items={blueprints.map((blueprint) => blueprint.name )} handleSelectedItemChange={(e) => setSelectedBlueprint(blueprints.find((blueprint, index) => blueprints[index].name === e.selectedItem ))} label={"Choose a blueprint"} />
+        {Object.keys(selectedBlueprint).length > 0 && <JsonView style={{paddingTop: "20px"}} data={selectedBlueprint} />}
+    </div>
+
+}
