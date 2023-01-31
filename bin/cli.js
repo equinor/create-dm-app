13fdc5912c707925aa440170c8046ee449d65e88
Ingerid Gjeitnes Hellen <ingerid.hellen@bouvet.no>
2023-01-31 14:54:49 +0100

    fix: remove unneeded files from copy
    Refs #61

--- bin/cli.js
+++ bin/cli.js
@@ -1,42 +1,46 @@
 #!/usr/bin/env node
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const {execSync} = require('child_process');
+const { execSync } = require('child_process')
 
-const runCommand = command => {
-    try {
-        execSync(`${command}`, {stdio: 'inherit'});
-    } catch (e) {
-        console.error('Failed to execute ${command}', e);
-        return false;
-    }
-    return true;
-};
-
-function sanitize_string(command) {
-    const regex_expression = /^[\w\d-_]+$/ //only allow letters (\w), digits (\d), dash (-) and underscore (_)
-    if (regex_expression.test(command)) {
-        return command
-    } else {
-        throw new Error("Please use a valid repo name (no special characters allowed).")
-    }
+const runCommand = (command) => {
+  try {
+    execSync(`${command}`, { stdio: 'inherit' })
+  } catch (e) {
+    console.error('Failed to execute ${command}', e)
+    return false
+  }
+  return true
 }
 
-const repoName = sanitize_string(process.argv[2]);
-const gitCheckoutCommand = `git clone --depth 1 https://github.com/equinor/create-dm-app ${repoName}`;
-const installDepsCommand = `cd ${repoName} && npm install`;
+function sanitize_string(command) {
+  const regex_expression = /^[\w\d-_]+$/ //only allow letters (\w), digits (\d), dash (-) and underscore (_)
+  if (regex_expression.test(command)) {
+    return command
+  } else {
+    throw new Error(
+      'Please use a valid repo name (no special characters allowed).'
+    )
+  }
+}
 
-console.log(`Cloning the repository with name ${repoName}`);
-const checkedOut = runCommand(gitCheckoutCommand);
-if (!checkedOut) process.exit(-1);
+const repoName = sanitize_string(process.argv[2])
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/equinor/create-dm-app ${repoName}`
+const installDepsCommand = `cd ${repoName} && npm install`
 
-console.log(`Installing dependencies for ${repoName}`);
-const installedDeps = runCommand(installDepsCommand);
-if (!installedDeps) process.exit(-1);
+console.log(`Cloning the repository with name ${repoName}`)
+const checkedOut = runCommand(gitCheckoutCommand)
+if (!checkedOut) process.exit(-1)
 
-console.log('Cleaning up...');
+console.log(`Installing dependencies for ${repoName}`)
+const installedDeps = runCommand(installDepsCommand)
+if (!installedDeps) process.exit(-1)
+
+console.log('Cleaning up...')
 runCommand(`rm -rf ${repoName}/bin`)
+runCommand(`rm -rf ${repoName}/.git`)
+runCommand(`rm -rf ${repoName}/.github`)
 
 console.log(
-    'Congratulations! You are ready. Follow the following commands to start'
-);
-console.log(`cd ${repoName} && npm start`);
+  'Congratulations! You are ready. Follow the following commands to start'
+)
+console.log(`cd ${repoName} && npm start`)
