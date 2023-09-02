2dcc8dd05f7997359a7f9412da32271e30856445
Eirik Ola Aksnes <eoaksnes@gmail.com>
2023-09-02 09:14:05 +0200

    build: replace create react app with vite

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import checker from 'vite-plugin-checker'
+import react from '@vitejs/plugin-react-swc'
+import viteTsConfigPaths from 'vite-tsconfig-paths'
+import svgrPlugin from 'vite-plugin-svgr'
+
+export default defineConfig({
+  base: '/',
+  plugins: [
+      checker({
+        typescript: true,
+      }),
+      react(),
+      viteTsConfigPaths(),
+      svgrPlugin(),
+   ],
+   define: {
+    'process.env': process.env
+  },
+  server: {
+    port: 3000,
+    host: true,
+  }
+})
