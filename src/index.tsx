25f0b3af871672ce724d5054fdf0a9312a2c2da0
Eirik Ola Aksnes <eoaksnes@gmail.com>
2023-09-07 08:20:06 +0200

    feat: add oauth auth support

--- src/index.tsx
+++ src/index.tsx
@@ -6,10 +6,36 @@ import {
 } from '@development-framework/dm-core'
 import App from './App'
 import { createRoot } from 'react-dom/client'
-
+import { AuthProvider } from 'react-oauth2-code-pkce'
 import plugins from './plugins'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+
+const fullCurrentURL = () =>
+  `${window.location.pathname}${window.location.search}${window.location.hash}`
+
+const authEnabled = import.meta.env.VITE_AUTH_ENABLED === '1'
+const authConfig = {
+  clientId: import.meta.env.VITE_CLIENT_ID,
+  authorizationEndpoint: `https://login.microsoftonline.com/${
+    import.meta.env.VITE_TENANT_ID
+  }/oauth2/v2.0/authorize`,
+  tokenEndpoint: `https://login.microsoftonline.com/${
+    import.meta.env.VITE_TENANT_ID
+  }/oauth2/v2.0/token`,
+  scope: import.meta.env.VITE_AUTH_SCOPE,
+  redirectUri: import.meta.env.VITE_REDIRECT_URI,
+  logoutEndpoint: `https://login.microsoftonline.com/${
+    import.meta.env.VITE_TENANT_ID
+  }/oauth2/logout`,
+  preLogin: () => localStorage.setItem('preLoginPath', fullCurrentURL()),
+  postLogin: () => {
+    if (localStorage.getItem('preLoginPath') !== fullCurrentURL()) {
+      // @ts-ignore
+      window.location.href = localStorage.getItem('preLoginPath')
+    }
+  },
+}
 
 const APP_SETTINGS = {
   visibleDataSources: [import.meta.env.VITE_DATA_SOURCE],
@@ -19,9 +45,8 @@ const APP_SETTINGS = {
 
 const container = document.getElementById('root')
 
-const root = createRoot(container!)
-root.render(
-  <React.StrictMode>
+const Content = () => {
+  return (
     <DMSSProvider>
       <ApplicationContext.Provider value={APP_SETTINGS}>
         <UiPluginProvider pluginsToLoad={plugins}>
@@ -30,5 +55,18 @@ root.render(
         </UiPluginProvider>
       </ApplicationContext.Provider>
     </DMSSProvider>
+  )
+}
+
+const root = createRoot(container!)
+root.render(
+  <React.StrictMode>
+    {authEnabled ? (
+      <AuthProvider authConfig={authConfig}>
+        <Content />
+      </AuthProvider>
+    ) : (
+      <Content />
+    )}
   </React.StrictMode>
 )
